50b8b51dc932f6ff6210f050b4093a448967e207
Mmesomachi <94153994+MathsMarshall@users.noreply.github.com>
2024-10-17 16:17:01 +0300

    Update Solution.py
    Fixed the bug of returning None for the grid once a user indicates an error and wishes to start from the beginning. Made the algorithm more robust for varying number of colours.

--- Solution.py
+++ Solution.py
@@ -1,60 +1,66 @@
 import time
-
-def visualise_input_so_far(grid):
-    rows = len(grid)
-    columns = len(grid[0])
-    out = ''
-    for i in range(9):
-        row = '\n'
-        for j in range(9):
-            if (i < rows) and (j < columns):
-                row += str(grid[i][j]) + ' '
-            else:
-                row += '_ '
-        out = out + row
-    print(out)
-    
+  
 def read_input():
     print('\nHello! Thank you for choosing to use this program to solve the LinkedIn Queen puzzle.')
     print('The algorithm was designed by Mmesomachi Nwachukwu. Contact at nwachukwummesomachi@gmail.com :).')
-    print('The computer is expecting 9 colours. We ask you to map this 9 colours to integers from 1 to 9 inclusive.')
+    temp = input('What is the size of the grid? Expecting a square grid. Please enter the number of rows. ')
+    while not temp.isdigit():
+        temp = input('What is the size of the grid? Expecting a square grid. Please enter the number of rows. ')
+    DOWN_BOUNDARY  = RIGHT_BOUNDARY = int(temp)
+    
+    def visualise_input_so_far(grid):
+        rows = len(grid)
+        columns = len(grid[0])
+        out = ''
+        for i in range(RIGHT_BOUNDARY):
+            row = '\n'
+            for j in range(RIGHT_BOUNDARY):
+                if (i < rows) and (j < columns):
+                    row += str(grid[i][j]) + ' '
+                else:
+                    row += '_ '
+            out = out + row
+        print(out)
+    
+    print(f'The computer is expecting {RIGHT_BOUNDARY} colours. We ask you to map this {RIGHT_BOUNDARY} colours to integers from 1 to {RIGHT_BOUNDARY} inclusive.')
     print('If no solution exists, it prints the puzzle without any Queen position.')
     print('Time to input the colours row by row')
     grid = []
-    for r in range(9):
+    check_set_input = set()
+    statement = 'What colour from'
+    for n in range(RIGHT_BOUNDARY):
+        check_set_input.add(str(n+1))
+        statement += ' ' + str(n+1) + ','
+    statement = statement[:-1] + ' is in '
+    for r in range(RIGHT_BOUNDARY):
         row = []
-        for c in range(9):
-            colour = input(f'What colour from 1, 2, 3, 4, 5, 6, 7, 8, 9, is in row {r+1} and column {c+1}? ')
-            while colour not in {'1', '2', '3', '4', '5', '6', '7', '8', '9'}:
-                colour = input(f'What colour from 1, 2, 3, 4, 5, 6, 7, 8, 9, is in row {r+1} and column {c+1}? Check again. ')
+        for c in range(RIGHT_BOUNDARY):
+            colour = input(statement + f'row {r+1} and column {c+1}? ')
+            while colour not in check_set_input:
+                colour = input(statement + f'row {r+1} and column {c+1}? Check again. ')
             row.append(int(colour))
         ifend = input('If you made a mistake and would like to start from beginning, press 0. To continue input as usual, press 1: ')
         if ifend == '0':
-            return
+            return None, RIGHT_BOUNDARY
         grid.append(row)
         visualise_input_so_far(grid)
-    return grid
+    return grid, RIGHT_BOUNDARY
 
-grid = read_input()
+grid, RIGHT_BOUNDARY = read_input()
     
-if not grid:
-    read_input()
+while not grid:
+    grid, RIGHT_BOUNDARY = read_input()
 
 colour_dict = {}
 position_dict = {}
-for i in range(9):
-    for j in range(9):
+for i in range(RIGHT_BOUNDARY):
+    for j in range(RIGHT_BOUNDARY):
         key = grid[i][j]
         position_dict[(i, j)] = key
         if grid[i][j] in colour_dict:
             colour_dict[key].add((i, j))
         else:
             colour_dict[key] = {(i, j)}
-
-LEFT_BOUNDARY  = 0
-RIGHT_BOUNDARY = 8
-UP_BOUNDARY    = 0
-DOWN_BOUNDARY  = 8
 
 def neighbours(position):
     row = position[0]
@@ -64,26 +70,18 @@ def neighbours(position):
         for j in range(col - 1, col + 2):
             if (i == row and j == col):
                 continue
-            if (UP_BOUNDARY   <= i <= DOWN_BOUNDARY and
-                LEFT_BOUNDARY <= j <= RIGHT_BOUNDARY):
+            if (0 <= i < RIGHT_BOUNDARY and
+                0 <= j < RIGHT_BOUNDARY):
                 neighbours.add((i, j))
     return neighbours
 
 def same_rowcolumn(position): # Returns set of positions in same row or column
     same = set()
-    for i in range(9):
-        for j in range(9):
+    for i in range(RIGHT_BOUNDARY):
+        for j in range(RIGHT_BOUNDARY):
             if (position[0] == i) or (position[1] == j):
                 same.add((i, j))
     return same
-
-# colour_dict = {}   # colours (int) as keys and positions it occupies
-# position_dict = {} # position as keys and colours (int) as values
-
-# Input is the list[list]. You should type the colour per row (comma seperated)
-# Example
-# Type the colours on row 1 :
-# purple, blue, blue, purple, orange, purple, green, green, purple
 
 def place_queen(position, old_dict):
     '''
@@ -97,11 +95,11 @@ def place_queen(position, old_dict):
         return -1, {}
     possibilities = {}
     colour = position_dict[(i, j)]
-    next_colour = 9
+    next_colour = RIGHT_BOUNDARY
     for key in old_dict:
         if key != colour:                               # Remove colour
             possibilities[key] = old_dict[key].copy()
-            possibilities[key] -= check_set             # Remove positions on same row or column or are neighbour of occupied position
+            possibilities[key] -= check_set             # Remove positions on the same row or column or are neighbours of occupied position
             next_colour = min(key, next_colour)
     return next_colour, possibilities
     
@@ -116,9 +114,9 @@ def listing(possibilities): # get set of possible positions
 def visualise(possibilities):
     out = ''
     possible_coordinates = listing(possibilities)
-    for i in range(9):
+    for i in range(RIGHT_BOUNDARY):
         row = '\n'
-        for j in range(9):
+        for j in range(RIGHT_BOUNDARY):
             if (i, j) not in possible_coordinates:
                 row += '0'
             else:
@@ -132,13 +130,13 @@ def play_game(colour = 1):
     
     def DFS(old_position, old_possibilities):
         l1 = len(old_possibilities)
-        if len(path) == 9:
+        if len(path) == RIGHT_BOUNDARY:
             return True, path
         next_colour, next_possibilities = place_queen(old_position, old_possibilities)
         l2 = len(next_possibilities)
         if not next_possibilities:
             path.add(old_position)
-            if len(path) == 9:
+            if len(path) == RIGHT_BOUNDARY:
                 return True, path
             return False, set()
         if ((l1 - l2) > 1) or (old_position in path): # A colour is prematurely eliminated or we encounter a position in path
@@ -148,7 +146,7 @@ def play_game(colour = 1):
         path.add(old_position)
         for next_position in recursion:
             condition = condition or DFS(next_position, next_possibilities)[0]
-            if len(path) == 9:
+            if len(path) == RIGHT_BOUNDARY:
                 return True, path
         path.remove(old_position)
         if condition:
@@ -161,9 +159,9 @@ def play_game(colour = 1):
 
 def visualise_solution(visited):
     out = ''
-    for i in range(9):
+    for i in range(RIGHT_BOUNDARY):
         row = '\n'
-        for j in range(9):
+        for j in range(RIGHT_BOUNDARY):
             if (i, j) in visited:
                 row += 'Q '
             else:
@@ -174,6 +172,6 @@ def visualise_solution(visited):
 sol = play_game()
 print('\nSolving...')
 time.sleep(1)
-print('Q represents Queen position')
+print('Q represents a Queen position')
 visualise_solution(sol)
-print('For more enquiries, please contact me at nwachukwummesomachi@gmail.com')
+print('\nFor more enquiries, please contact me at nwachukwummesomachi@gmail.com')
